4fe80b411e562d932a33e583b475d7ee11417ace
Hassan Ali <7sn3li2005@gmail.com>
2026-08-29 18:31:11 +0300

    Clear implementation; keep structure only

--- src/models/review.py
+++
@@ -1,6 +0,0 @@
-class Review:
-    def __init__(self, customer_id: int, product_id: int, rating: float, comment: str = ""):
-        self.customer_id = customer_id
-        self.product_id = product_id
-        self.rating = rating
-        self.comment = comment
