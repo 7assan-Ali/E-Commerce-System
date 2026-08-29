57d56fe86acfe523a637cde1dfac4f44e7966ada
Hassan Ali <7sn3li2005@gmail.com>
2026-08-29 18:27:43 +0300

    Add Address model

---
+++ src/models/address.py
@@ -0,0 +1,7 @@
+class Address:
+    def __init__(self, address_id: int, street: str, city: str, governorate: str, postal_code: str):
+        self.address_id = address_id
+        self.street = street
+        self.city = city
+        self.governorate = governorate
+        self.postal_code = postal_code
