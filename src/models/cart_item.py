8fd6ec1e7b1cd86a93379ef3217cb9219a31c553
Hassan Ali <7sn3li2005@gmail.com>
2026-08-29 18:30:41 +0300

    Clear implementation; keep structure only

--- src/models/cart_item.py
+++
@@ -1,8 +0,0 @@
-class CartItem:
-    def __init__(self, product, quantity: int):
-        self.product = product
-        self.quantity = quantity
-        self.unit_price = product.price
-
-    def subtotal(self) -> float:
-        return self.quantity * self.unit_price
